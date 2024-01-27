ff14fe8584482c6500a97568ac65888ea0f51982
gen740 <gen740.f@gmail.com>
2024-01-28 03:04:13 +0900

    [CI] Build ubuntu instead of arch

---
+++ ubuntu-22.04/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+apt-get -y install tzdata
+
+# Neovim dependencies
+# https://github.com/neovim/neovim/blob/master/BUILD.md#ubuntu--debian
+apt-get -y install ninja-build gettext cmake unzip curl
+
+# Pyenv dependencies
+# https://github.com/pyenv/pyenv/wiki
+apt-get -y install build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev curl \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+# Tools
+apt-get -y install wget git zsh tmux fd-find ripgrep aptitude
+
+## locale configuration
+ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+yes | apt-get install -y sudo
