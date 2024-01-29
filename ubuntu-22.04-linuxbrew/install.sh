cd37a590f0148ea5a6f85fda28adf212ba2fa0a4
gen740 <gen740.f@gmail.com>
2024-01-29 09:36:57 +0900

    [Update] Build ubuntu linuxbrew

---
+++ ubuntu-22.04-linuxbrew/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -ex
+
+apt-get -y update && apt-get -y upgrade
+
+# Locale dependencies
+apt-get -y install tzdata locales
+
+# LLVM latest
+apt-get -y install lsb-release wget software-properties-common gnupg
+wget -q https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+./llvm.sh 18 all
+rm llvm.sh
+
+# Neovim dependencies
+# https://github.com/neovim/neovim/blob/master/BUILD.md#ubuntu--debian
+apt-get -y install ninja-build gettext cmake unzip curl
+
+# Pyenv dependencies
+# https://github.com/pyenv/pyenv/wiki
+apt-get -y install build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev curl \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+# Tools
+apt-get -y install wget git zsh tmux fd-find ripgrep aptitude
+ln -sf /bin/fdfind /usr/local/bin/fd
+apt-get -y install direnv
+
+## locale configuration
+ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+sed -i '/^#.*en_US.UTF-8/s/^#//' /etc/locale.gen
+locale-gen
+yes | apt-get install -y sudo
+
+apt-get clean
+apt-get autoremove -y
